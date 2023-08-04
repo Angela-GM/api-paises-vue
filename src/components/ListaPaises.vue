992c44821497fcec7a29d2f27b2273b884b3f9d7
Angela-GM <angelagm0112@gmail.com>
2023-08-04 23:09:03 +0200

    mostrar una lista con los nombres de todos los paises

--- src/components/ListaPaises.vue
+++ src/components/ListaPaises.vue
@@ -1,28 +1,41 @@
-<template>
-    <h1>Listado de Paises</h1>
-    
-</template>
-
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
+
+const data = ref([]);
+
 
 onMounted(async () => {
   try {
     const response = await fetch('https://restcountries.com/v3.1/all');
-    const data = await response.json();
+    data.value = await response.json();
     console.log(data);
     
   } catch (error) {
     console.error('Error fetching data:', error);
   }
-});
-
-
-    
+}); 
 
 
 
 </script>
+
+
+<template>
+    <div>
+        <h1>Lista de Paises</h1>
+        <ul>
+        <li v-for="pais in data" :key="pais.name.common">
+            {{ pais.name.common }}
+        </li>
+        </ul>
+    </div>
+
+    
+ 
+    
+</template>
+
+
 
 
 <style scoped></style>
