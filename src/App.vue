abd5a3e98613d1e7c76bc65a91e7df43c1dfaab2
Angela-GM <angelagm0112@gmail.com>
2023-08-04 16:17:10 +0200

    arreglar tsconfig, y consumir datos de la api con fetch

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,14 @@
 <script setup lang="ts">
 import './index.css'
+import ListaPaises from './components/ListaPaises.vue';
 </script>
 
 <template>
-  <div>
-    <h1 class="text-red-500">Hello Tailwind</h1>
+  <ListaPaises />
+  
 
-  </div>
   </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ src/components/ListaPaises.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1>Listado de Paises</h1>
+    
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+onMounted(async () => {
+  try {
+    const response = await fetch('https://restcountries.com/v3.1/all');
+    const data = await response.json();
+    console.log(data);
+    
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+});
+
+
+    
+
+
+
+</script>
+
+
+<style scoped></style>
